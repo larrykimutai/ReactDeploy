34dcc787a7a385e80878c06d4ddb5c5d9033317f
truejv <jdvaughn@aggies.ncat.edu>
2022-09-29 16:08:07 -0400

    Finished the refund function

--- Smart Contract/default_workspace/contracts/senior project.sol
+++ Smart Contract/default_workspace/contracts/senior project.sol
@@ -13,11 +13,12 @@ contract MyContract{
     struct donor{
             uint totalDono;
             uint returnedDono;
+            uint returnable;
             uint donorListPointer;
     }
 
     mapping(address => donor) public balances;
-    address[] public donorList;
+    address payable[] public donorList;
 
     event LogReceivedFunds(address sender, uint amount);
     event LogReturnedFunds(address recipient, uint amount);
@@ -28,25 +29,31 @@ contract MyContract{
         return donorList.length;
         }
     
-    
 
     function donate() public payable{
-        //balances[msg.sender] += 1;
         balances[msg.sender].totalDono += msg.value;
+        balances[msg.sender].returnable += msg.value;
         balances[msg.sender].donorListPointer = Dcounter++;
-        donorList.push(msg.sender);
+        donorList.push(payable(msg.sender));
         emit LogReceivedFunds(msg.sender, msg.value);
     }
 
     //this function sends the funds after requirements are met
     function sendFunds () public{
         wallet.transfer(balances[msg.sender].totalDono); 
-        //balances[msg.sender].totalDono = 0;
+        balances[msg.sender].returnable == 0;
     }
 
-    //this function returns funds after requirements are not met
+    //this function returns funds if requirements are not met
     function returnFunds() public payable{
-        balances[msg.sender].returnedDono += msg.value;
+        for(uint i; i < donorList.length-1; i++){
+            //checks if the user has any ether remaining in the contract 
+            if(balances[donorList[i]].returnable > 0){
+                donorList[i].transfer(balances[donorList[i]].returnable);
+                balances[donorList[i]].returnedDono == balances[msg.sender].totalDono;
+                balances[donorList[i]].returnable == 0;
+            }
+        }
         emit LogReturnedFunds(msg.sender, msg.value);
     }
 
