a6e408864b79a15ec3902f8f6de8354e0ecc99d9
truejv <jdvaughn@aggies.ncat.edu>
2022-10-11 19:04:15 -0400

    Mid Stage

--- Smart Contract/default_workspace/contracts/senior project.sol
+++ Smart Contract/default_workspace/contracts/senior project.sol
@@ -4,10 +4,15 @@ pragma solidity >= 0.5.1;
 contract MyContract{
     address payable wallet;
     uint Dcounter;
-    
+    uint deployDate;//start date
+    uint deadline;//the amount of weeks the project will take
+    //this boolean represents user approval of work done
+    bool approved = false;
 
-    constructor (address payable _wallet) {
+    constructor (address payable _wallet, uint numWeeks) {
         wallet = _wallet;
+        deployDate = block.timestamp;
+        deadline = numWeeks;
     }
 
     struct donor{
@@ -22,13 +27,13 @@ contract MyContract{
 
     event LogReceivedFunds(address sender, uint amount);
     event LogReturnedFunds(address recipient, uint amount);
-
+/*
     function getDonorCount()
-        public 
+         public 
         returns(uint donorCount){
         return donorList.length;
         }
-    
+    */
 
     function donate() public payable{
         balances[msg.sender].totalDono += msg.value;
@@ -41,20 +46,47 @@ contract MyContract{
     //this function sends the funds after requirements are met
     function sendFunds () public{
         wallet.transfer(balances[msg.sender].totalDono); 
-        balances[msg.sender].returnable == 0;
+        balances[msg.sender].returnable = 0;
     }
 
+    function singleRefund () public{
+        uint returnAmt = balances[msg.sender].returnable;
+        payable (msg.sender).transfer(returnAmt);
+        emit LogReturnedFunds(msg.sender, returnAmt);
+    } 
+
     //this function returns funds if requirements are not met
-    function returnFunds() public payable{
-        for(uint i; i < donorList.length-1; i++){
+    function returnFunds() private {
+        for(uint i; i <= donorList.length-1; i++){
+            uint returnAmt;
             //checks if the user has any ether remaining in the contract 
             if(balances[donorList[i]].returnable > 0){
-                donorList[i].transfer(balances[donorList[i]].returnable);
-                balances[donorList[i]].returnedDono == balances[msg.sender].totalDono;
-                balances[donorList[i]].returnable == 0;
+                returnAmt = balances[donorList[i]].returnable;
+                donorList[i].transfer(returnAmt);
+                balances[donorList[i]].returnedDono = returnAmt;
+                balances[donorList[i]].returnable = 0;
+                emit LogReturnedFunds(donorList[i], returnAmt);
+                }
             }
         }
-        emit LogReturnedFunds(msg.sender, msg.value);
+    
+    function approval() private{       
+      require(msg.sender == wallet);
+        approved = true;
     }
+
+    function timeExpired() public {
+        //checks if the time expired and the creator uploaded proof of work
+        if(block.timestamp >= (deployDate + (deadline * 1 weeks))
+        && approved == true){
+            selfdestruct(wallet);
+        }
+    //the refund function is called if the deadline is met
+        else if (block.timestamp >= (deployDate + (deadline * 1 weeks))
+        && approved == false){
+            returnFunds();
+    }
+  }
+
 
 }
