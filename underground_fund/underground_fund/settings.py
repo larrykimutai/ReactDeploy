0449bac61985282285247235078b6d6b3d6ceea2
Sean Tisdale <setisdale@aggies.ncat.edu>
2022-11-06 16:44:02 -0500

    removed error

--- underground_fund/underground_fund/settings.py
+++ underground_fund/underground_fund/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tk8tyr5km#2^6$1t$nywqx@@t&+bs3&&m_f2w=0ryza2oa^hf2'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
